4e8209b0ef903472c3f560b9c95ae6794ed08c81
Hasan Can <tepehasan342@gmail.com>
2023-05-08 17:26:41 +0300

    Using part and part of in flutter
    The "part" statement is used to run code within one file into another file. This can make the file of a large file more manageable, allowing you to split the code into smaller chunks.
    On the other hand, the phrase "Part of" is used to indicate which class or module a file belongs to. This allows you to collect all the code of a particular class or module in a single file, making the code more organized and easily manageable.

---
+++ flutter_part_partof/android/app/src/main/kotlin/com/example/flutter_part_partof/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_part_partof
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_part_partof/lib/part/part_scaffold.dart
@@ -0,0 +1,14 @@
+part of './part_of_learn.dart';
+
+class _MyContainer extends StatelessWidget {
+  const _MyContainer();
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 100,
+      height: 200,
+      child: Center(child: Text("Test")),
+    );
+  }
+}
